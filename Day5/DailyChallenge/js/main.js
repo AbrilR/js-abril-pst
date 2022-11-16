ca84992385c39ce57187c58ab8562465a4b50125
AbrilR <abrilramirez310@gmail.com>
2022-11-16 15:54:06 -0800

    Day5

---
+++ Day5/DailyChallenge/js/main.js
@@ -0,0 +1,12 @@
+//input from the user
+const number = prompt("Please enter a number: ");
+
+//check if number is even
+if(number % 2 == 0) {
+    alert ('The number is even.');
+}
+//if number is odd
+else {
+    alert ('The number is odd.');
+}
+evenOrOdd(6);
