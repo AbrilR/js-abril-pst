59d5d12e16fe2f0b750522d39a7e834e6ce2d363
AbrilR <abrilramirez310@gmail.com>
2022-12-04 15:08:02 -0800

    Day3 DailyChallenge

--- Day3/DailyChallenge/js/main.js
+++ Day3/DailyChallenge/js/main.js
@@ -1,13 +1,29 @@
-function myFunction() {
+// function myFunction() {
 
-  let text;
-  let person = prompt("Please enter your name:");
+//   let text;
+//   let person = prompt("Please enter your name:");
 
-  if (person == null || person == "") {
-      text = "User cancelled the prompt.";
-  } else {
-      text = "Hello " + person + "! How are you today?";
+//   if (person == null || person == "") {
+//       text = "User cancelled the prompt.";
+//   } else {
+//       text = "Hello " + person + "! How are you today?";
+//   }
+
+//   document.getElementById("demo").innerHTML = text;
+// }
+
+function reversingString(str) {
+        
+  // empty string
+  let aNewString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+      aNewString += str[i];
   }
-
-  document.getElementById("demo").innerHTML = text;
+  return aNewString;
 }
+      
+// take input from the user
+const string = prompt('What is your name?');
+      
+const result = reversingString(string);
+alert("Hello " + result + "! How are you today?");
