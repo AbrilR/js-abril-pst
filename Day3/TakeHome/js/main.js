3fb7306fff0eceae3c7ac40c87804aee49a0ca3d
AbrilR <abrilramirez310@gmail.com>
2022-12-04 15:27:15 -0800

    Day3 DailyChallenge

---
+++ Day3/TakeHome/js/main.js
@@ -0,0 +1,7 @@
+function welcomeTheUser() {
+
+    let firstName = document.getElementById("first-name").value; 
+    let lastName= document.getElementById("last-name").value;
+    console.log(firstName);
+    alert("Welcome "+ lastName);
+}
