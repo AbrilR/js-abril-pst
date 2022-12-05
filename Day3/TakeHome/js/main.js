b2f1089dbaf55f6778c8313d23fdab1f6c422c12
AbrilR <abrilramirez310@gmail.com>
2022-12-05 10:13:25 -0800

    Day3 TakeHome

--- Day3/TakeHome/js/main.js
+++ Day3/TakeHome/js/main.js
@@ -5,3 +5,31 @@ function welcomeTheUser() {
     console.log(firstName);
     alert("Welcome "+ lastName);
 }
+
+
+
+function myFunction() {
+
+  let text;
+  let birthday = prompt("Please enter your birthday:");
+
+  if (birthday == null || birthday == "") {
+      text = "User cancelled the prompt.";
+  } else {
+      text = "Your birthday is " + birthday
+  }
+
+  document.getElementById("demo").innerHTML = text;
+}
+
+
+
+let YOB = prompt('What is your Year of Birth?', 1900);
+
+const currentYear = new Date().getFullYear();
+
+if(YOB === null) {
+    YOB = ''
+}
+
+alert("`You are ＄{currentYear - YOB} years old!`" + YOB); // You are 100 years old!
