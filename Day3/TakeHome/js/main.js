7ac2063c600266a05d5f78fc134187970689af99
AbrilR <abrilramirez310@gmail.com>
2022-12-07 11:58:46 -0800

    Day3 TakeHome

--- Day3/TakeHome/js/main.js
+++ Day3/TakeHome/js/main.js
@@ -2,6 +2,7 @@ function welcomeTheUser() {
 
     let firstName = document.getElementById("first-name").value; 
     let lastName= document.getElementById("last-name").value;
+    
     console.log(firstName);
     alert("Welcome "+ lastName);
 }
